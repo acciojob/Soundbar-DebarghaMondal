217e62eeb6d374df830d325dc9126cbec8d49b3a
Debargha Mondal <166617838+DebarghaMondal@users.noreply.github.com>
2024-08-04 11:08:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+document.getElementById('sound1').addEventListener('click', function() {
+   // Code to play sound1
+	var sound1 = new Audio('sounds/sound1.mp3');
+sound1.play();
+});
+
+document.querySelector('.stop').addEventListener('click', function() {
+   // Code to stop all sounds
+	sound1.pause();
+sound1.currentTime = 0;
+});
